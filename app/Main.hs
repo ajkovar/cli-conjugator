31e26e90c7b32ab0b4c2dc36db4ccf2cbb13b477
Alex Kovar <alexkovar@MacBook-Air-de-Alex.local>
2024-01-22 16:50:48 -0600

    init

---
+++ app/Main.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad
+import Data.List (find)
+import qualified Data.Text as T
+import Database.SQLite.Simple
+
+data NaturalWord = NaturalWord
+  { infinitive :: String,
+    mood :: String,
+    tense :: String,
+    translation :: String,
+    fps :: String,
+    sps :: String,
+    tps :: String,
+    fpp :: String,
+    spp :: String,
+    tpp :: String
+  }
+  deriving (Show)
+
+instance FromRow NaturalWord where
+  fromRow = NaturalWord <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
+
+moods :: [String]
+moods = ["Indicativo", "Subjuntivo", "Imperativo Afirmativo", "Imperativo Negativo"]
+
+persons :: [NaturalWord -> String]
+persons = [fps, sps, tps, fpp, spp, tpp]
+
+tenses :: [String]
+tenses = ["Presente", "Pretérito", "Imperfecto", "Imperfecto", "Futuro"]
+
+l :: [String]
+l = ["corro", "corrí", "corría", "correría", "correré"]
+
+padR :: Int -> String -> String
+padR n s
+  | length s < n = s ++ replicate (n - length s) ' '
+  | otherwise = s
+
+formatLine :: [String] -> String
+formatLine line = foldl1 (++) (map (padR 15) line)
+
+filterTense :: [NaturalWord] -> String -> [NaturalWord]
+filterTense nws t = filter (\nw -> tense nw == t) nws
+
+tensesForPerson :: (NaturalWord -> String) -> [NaturalWord] -> String
+tensesForPerson personFn nws = formatLine $ map personFn $ concat $ map (filterTense nws) tenses
+
+printMood :: [NaturalWord] -> String -> IO ()
+printMood nws m = do
+  putStrLn $ "\n" ++ m ++ "\n"
+  putStrLn $ formatLine $ filter (\t -> length (filterTense indicatives t) > 0) tenses
+  mapM_ (\personFn -> putStrLn $ tensesForPerson personFn indicatives) persons
+  where
+    indicatives = filter (\nw -> mood nw == m) nws
+
+main :: IO ()
+main = do
+  conn <- open "conjugation.db"
+
+  rows <- query_ conn "SELECT * from verbs where infinitive=\"correr\"" :: IO [NaturalWord]
+  mapM_ (printMood rows) moods
+
+  close conn
