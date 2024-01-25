1d84a32ff9cb9b97ff0896d43876e7a08e2bc862
Alex Kovar <alexkovar@MacBook-Air-de-Alex.local>
2024-01-25 10:41:30 -0600

    added more labels

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,19 @@
 
 import Control.Monad
 import Data.List (find)
-import Database.SQLite.Simple 
+import Database.SQLite.Simple
 import System.Environment (getArgs)
+
+type Mood = String
+
+type Tense = String
+
+type Prop = NaturalWord -> String
 
 data NaturalWord = NaturalWord
   { infinitive :: String,
-    mood :: String,
-    tense :: String,
+    mood :: Mood,
+    tense :: Tense,
     translation :: String,
     fps :: String,
     sps :: String,
@@ -22,43 +28,41 @@ data NaturalWord = NaturalWord
 instance FromRow NaturalWord where
   fromRow = NaturalWord <$> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field <*> field
 
-type Mood = String
-type Tense = String
-
 moods :: [Mood]
 moods = ["Indicativo", "Subjuntivo", "Imperativo Afirmativo", "Imperativo Negativo"]
 
-persons :: [NaturalWord -> String]
-persons = [fps, sps, tps, fpp, spp, tpp]
+persons :: [(String, Prop)]
+persons = [("Yo", fps), ("Tú", sps), ("él/ella/Ud.", tps), ("nosotros", fpp), ("vosotros", spp), ("ellos/ellas/Uds.", tpp)]
 
 tenses :: [Tense]
 tenses = ["Presente", "Pretérito", "Imperfecto", "Imperfecto", "Futuro"]
- 
+
 padR :: Int -> String -> String
 padR n s
   | length s < n = s ++ replicate (n - length s) ' '
   | otherwise = s
 
 formatLine :: [String] -> String
-formatLine line = foldl1 (++) (map (padR 15) line)
+formatLine line = foldl1 (++) (map (padR 16) line)
 
 filterTense :: [NaturalWord] -> Tense -> [NaturalWord]
 filterTense nws t = filter ((== t) . tense) nws
 
-tensesForPerson :: (NaturalWord -> String) -> [NaturalWord] -> String
-tensesForPerson personFn nws = formatLine $ map personFn $ concat $ map (filterTense nws) tenses
-
 printMood :: [NaturalWord] -> Mood -> IO ()
 printMood nws m = do
   putStrLn $ "\n" ++ m ++ "\n"
-  putStrLn $ formatLine $ filter (\t -> length (filterTense currentMood t) > 0) tenses
-  mapM_ (\personFn -> putStrLn $ tensesForPerson personFn currentMood) persons
+  putStrLn $ formatLine $ filter (\t -> length (filterTense moodMatches t) > 0) tenses
+  mapM_ printPerson persons
   where
-    currentMood = filter (\nw -> mood nw == m) nws
+    moodMatches = filter ((== m) . mood) nws
+    tenseValues = concat $ map (filterTense moodMatches) tenses
+    personLabels = map fst persons
+    tensesForPerson personFn = map personFn tenseValues
+    printPerson (label, personFn) = putStrLn $ formatLine $ label:tensesForPerson personFn
 
 main :: IO ()
 main = do
-  verb:args <- getArgs 
+  verb : args <- getArgs
   conn <- open "conjugation.db"
 
   rows <- query conn "SELECT * from verbs where infinitive=?" (Only (verb :: String)) :: IO [NaturalWord]
